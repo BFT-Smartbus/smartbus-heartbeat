893f6b202a52152d20132a2a83cc7126b55bd526
Chao Zhou <Czhou0k@gmail.com>
2022-12-30 13:18:22 -0500

    got a working yaml file and imported yaml to create table py

--- create_table.py
+++ create_table.py
@@ -1,18 +1,37 @@
 from flask import Flask
 import boto3
+import yaml
+from yaml import load, dump
 
-REGION_NAME = "us-east-1"
-ENDPOINT_URL = "https://dynamodb.us-east-1.amazonaws.com/"
-TABLE_NAME = "heartbeat"
-KEYSCHEMA = [
-        {"AttributeName": "user_id", "KeyType": "HASH"},
-        {"AttributeName": "time_stamp", "KeyType": "RANGE"},
-    ],
-ATTRIBUTEDEFINITIONS = [
-        {"AttributeName": "user_id", "AttributeType": "S"},
-        {"AttributeName": "time_stamp", "AttributeType": "N"},
-    ]
-PROVISIONEDTHROUGHPUT = {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+app = Flask(__name__)
+
+with open("settings.yaml", "r") as stream:
+    data = load(stream, Loader=Loader)
+
+
+print(data)
+
+
+
+
+
+# REGION_NAME = "us-east-1"
+# ENDPOINT_URL = "https://dynamodb.us-east-1.amazonaws.com/"
+# TABLE_NAME = "heartbeat"
+# KEYSCHEMA = [
+#         {"AttributeName": "user_id", "KeyType": "HASH"},
+#         {"AttributeName": "time_stamp", "KeyType": "RANGE"},
+#     ],
+# ATTRIBUTEDEFINITIONS = [
+#         {"AttributeName": "user_id", "AttributeType": "S"},
+#         {"AttributeName": "time_stamp", "AttributeType": "N"},
+#     ]
+# PROVISIONEDTHROUGHPUT = {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
 
 app = Flask(__name__)
 
@@ -22,14 +41,16 @@ app = Flask(__name__)
 
 dynamodb = boto3.resource(
     "dynamodb",
-    region_name= REGION_NAME,
-    endpoint_url= ENDPOINT_URL,
+    region_name = data['REGION_NAME'],
+    endpoint_url= data['ENDPOINT_URL']
 )
 
+
+
 # create a dynamodb table named heartbeat, with a partition key:user_id, and a range key: time_stamp
-table = dynamodb.create_table(
-    TableName= TABLE_NAME,
-    KeySchema= KEYSCHEMA,
-    AttributeDefinitions= ATTRIBUTEDEFINITIONS,
-    ProvisionedThroughput= PROVISIONEDTHROUGHPUT,
-)
+# table = dynamodb.create_table(
+#     TableName= TABLE_NAME,
+#     KeySchema= KEYSCHEMA,
+#     AttributeDefinitions= ATTRIBUTEDEFINITIONS,
+#     ProvisionedThroughput= PROVISIONEDTHROUGHPUT,
+# )
