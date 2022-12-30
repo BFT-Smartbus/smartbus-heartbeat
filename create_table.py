c7388a66fc5fac0b8ac32d4e8c71becc95893776
Chao Zhou <Czhou0k@gmail.com>
2022-12-30 13:25:27 -0500

    cleanup create_table file and got rid of unused code and yaml is working

--- create_table.py
+++ create_table.py
@@ -14,29 +14,7 @@ with open("settings.yaml", "r") as stream:
     data = load(stream, Loader=Loader)
 
 
-print(data)
-
-
-
-
-
-# REGION_NAME = "us-east-1"
-# ENDPOINT_URL = "https://dynamodb.us-east-1.amazonaws.com/"
-# TABLE_NAME = "heartbeat"
-# KEYSCHEMA = [
-#         {"AttributeName": "user_id", "KeyType": "HASH"},
-#         {"AttributeName": "time_stamp", "KeyType": "RANGE"},
-#     ],
-# ATTRIBUTEDEFINITIONS = [
-#         {"AttributeName": "user_id", "AttributeType": "S"},
-#         {"AttributeName": "time_stamp", "AttributeType": "N"},
-#     ]
-# PROVISIONEDTHROUGHPUT = {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
-
 app = Flask(__name__)
-
-# use boto3, a AWS SDK for Python to appoint which region to find your dynamodb table
-
 
 
 dynamodb = boto3.resource(
@@ -45,12 +23,12 @@ dynamodb = boto3.resource(
     endpoint_url= data['ENDPOINT_URL']
 )
 
+table = dynamodb.create_table(
+    TableName= data['TABLE_NAME'],
+    KeySchema= data['KEYSCHEMA'],
+    AttributeDefinitions= data['ATTRIBUTEDEFINITIONS'],
+    ProvisionedThroughput= data['PROVISIONEDTHROUGHPUT'],
+)
 
 
-# create a dynamodb table named heartbeat, with a partition key:user_id, and a range key: time_stamp
-# table = dynamodb.create_table(
-#     TableName= TABLE_NAME,
-#     KeySchema= KEYSCHEMA,
-#     AttributeDefinitions= ATTRIBUTEDEFINITIONS,
-#     ProvisionedThroughput= PROVISIONEDTHROUGHPUT,
-# )
+
