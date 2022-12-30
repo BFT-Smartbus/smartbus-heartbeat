e4e575c55da795e13517ec22e608c6ba901bae49
Chao Zhou <Czhou0k@gmail.com>
2022-12-30 14:22:44 -0500

    load data to dynamodb

--- dynamodb_loader.py
+++ dynamodb_loader.py
@@ -3,24 +3,28 @@ import json
 from decimal import Decimal
 from yaml import load
 
-# try:
-#     from yaml import CLoader as Loader
-# except ImportError:
-#     from yaml import Loader
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
 
-# with open("settings.yaml", "r") as stream:
-#     data = load(stream, Loader=Loader)
+#load constant values from setting.yaml
+with open("settings.yaml", "r") as stream:
+    data = load(stream, Loader=Loader)
 
-
-
-# dynamodb = boto3.resource('dynamodb', data['REGION_NAME'])
-dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
+#use boto3, a AWS SDK for Python to appoint which region to find your dynamodb table
+dynamodb = boto3.resource('dynamodb', data['REGION_NAME'])
 
 table = dynamodb.Table('heartbeat')
 
+#open the JSON file that has some heartbeat testing data, load this JSON file
 with open('heartbeat_data.json') as json_file:
   heartbeats = json.load(json_file)
+
+  #convert JSON data where it has float data structure to decimal data structure since AWS DynamoDB does not support float type, and convert it to a dictionary
   heartbeat_converted = json.loads(json.dumps(heartbeats), parse_float=Decimal)
+
+  #loop over the heartbeat list, and write all heartbeat data to dynamoDB heartbeat table.
   for heartbeat in heartbeat_converted:
     user_id = heartbeat['user_id']
     time_stamp = heartbeat['time_stamp']
