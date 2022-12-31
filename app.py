a677ec04b768273f04266182fe9b9d0eec049c05
Chao Zhou <Czhou0k@gmail.com>
2022-12-31 13:27:20 -0500

    added post endpoint to dynamodb

--- app.py
+++ app.py
@@ -28,6 +28,39 @@ def get_all_heartbeat():
     # logger.info("All heartbeat data returned")
     return jsonify(response)
 
+@app.route("/heartbeat", methods=["POST"])
+@cross_origin()
+def heartbeatpost():
+    data = json.loads(request.get_data())
+    userId = data["userId"]
+    timestamp = data["timestamp"]
+    latitude = data["latitude"]
+    longitude = data["longitude"]
+    speed = data["speed"]
+
+    if (
+        not userId
+        or not timestamp
+        or not latitude
+        or not longitude
+    ):
+        return "Unable to write"
+
+    post_heartbeat(userId, timestamp, latitude, longitude, speed)
+    return "Heartbeat data added successfully", 200
+
+def post_heartbeat(id, timestamp, lat, long, speed):
+
+    table.put_item(
+        Item={
+            "userId": id,
+            "timestamp": timestamp,
+            "latitude": lat,
+            "longitude": long,
+            "speed": speed,
+        }
+    )
+
 # @app.route("/heartbeat", methods=["POST"])
 # @cross_origin()
 # def heartbeatpost():
